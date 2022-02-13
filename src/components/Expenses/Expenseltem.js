0ca07b76dcdefc10f3e320cd62748568fdd85b78
BingBing <hwangwonbins@gmail.com>
2022-02-14 00:37:45 +0900

    Click Handler Start

--- src/components/Expenses/Expenseltem.js
+++ src/components/Expenses/Expenseltem.js
@@ -3,13 +3,20 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  let title = props.title;
+
+  const clickHandler = () => {
+    title = "Updated!";
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <p className="expense-item__price">${props.amount}</p>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
